37025fa2d226f16314c55404370950f844a710a9
AadhilMoola <aakmoola@gmail.com>
2022-06-08 15:22:52 +0200

    Coded function computerPlay

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-prompt("Rock, Paper or Scissors?")
+function computerPlay(){
+    let randomNumber = Math.floor((Math.random()*10)+1);
+console.log(randomNumber)
+    if(randomNumber>3 && randomNumber<6){
+        return "rock"
+    }else if(randomNumber>6 && randomNumber<10){
+        return "scissors"
+    }else{
+        return "paper"
+    }
+}
+
+console.log(computerPlay())
