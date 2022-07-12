796ada07922068dda43fa364bd086472918e8b56
AadhilMoola <aakmoola@gmail.com>
2022-07-12 07:02:05 +0200

    Created working UI

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Caching the DOM
+let body = document.querySelector('body')
+let header_div = document.querySelector('.header')
+let heading_h1 = document.querySelector('.heading')
+let heading_div = document.querySelector('heading2')
+let main_div = document.querySelector('.main')
+let rock_button = document.querySelector('.rock')
+let paper_button = document.querySelector('.paper')
+let scissors_button = document.querySelector('.scissors')
+let winner_div = document.querySelector('.winner')
+let scoreboard_div = document.querySelector('.score')
+let userScore_span = document.querySelector('.userScore')
+let computerScore_span = document.querySelector('.computerScore')
+let footer_div = document.querySelector('.footer')
+
 //Lets computer pick a random number, this number determines whether Rock, Paper or Scissors is chosen
 function computerPlay(){
     let randomNumber = Math.floor(Math.random()*3+1);
@@ -14,12 +29,25 @@ function computerPlay(){
 let playerScore = 0,
     computerScore = 0;
 
+//prompt to get user selection stored here
+//function playerSelection(){
+    //commented out player selection for console ui
+    //let playerChoice = prompt("Rock , Paper , Scissors");
+    
+    //let playerChoice = 
+    //return playerChoice;
+
+    
+//}
+
+
+
+
+
 //Determines the results based on user selection and computer selection
 function playRound(playerSelection, computerSelection){
         
-    
-    
-    if(playerSelection==computerSelection){
+    if(playerSelection===computerSelection){
         return("It's a Tie")
     } else if(
         (playerSelection=="rock"&&computerSelection=="scissors")||
@@ -34,17 +62,53 @@ function playRound(playerSelection, computerSelection){
 
 }
 
-//prompt to get user selection stored here
-function playerSelection(){
-    let playerChoice = prompt("Rock , Paper , Scissors");
-    console.log(`You Chose ${playerChoice}`)
-    return playerChoice.toLowerCase();
-    
+//create function that runs a game of 5
+
+function fiveround(){
+if(playerScore===5){
+    userScore_span.textContent = `User: ${playerScore}`
+    computerScore_span.textContent = `Computer: ${computerScore}`
+    winner_div.textContent = 'YOU WIN'
+}else if(computerScore===5){
+    userScore_span.textContent = `User: ${playerScore}`
+    computerScore_span.textContent = `Computer: ${computerScore}`
+    winner_div.textContent ='YOU LOSE'
+}else{
+    userScore_span.textContent = `User: ${playerScore}`
+    computerScore_span.textContent = `Computer: ${computerScore}`
+}
 }
 
 
+//runs one game
+rock_button.addEventListener('click', function(){
+    playRound('rock', computerPlay())
+    console.log('You Chose Rock')
+    fiveround()
+})
 
-function game(){
+paper_button.addEventListener('click', function(){
+    playRound('paper', computerPlay())
+    console.log('You Chose Paper')
+    fiveround()
+})
+
+scissors_button.addEventListener('click', function(){
+    playRound('scissors', computerPlay())
+    console.log('You Chose Scissors')
+    fiveround()
+})
+
+
+// Adding the Score
+
+
+
+
+
+
+//runs the game
+/* function game(){
 for(let i=0; i<5; i++){
 console.log(playRound(playerSelection(), computerPlay()))
 console.log(`Score is ${playerScore} : ${computerScore}`)
@@ -54,6 +118,5 @@ if(playerScore>computerScore){
 }else{
     console.log("Best out of 5: YOU LOSE :(")
 }
-}
-
-game()
+} 
+ */
